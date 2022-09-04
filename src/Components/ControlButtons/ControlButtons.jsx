cfa0fe5d3b2eba30173f19a529be73dafb947470
Your Name <you@example.com>
2022-09-05 01:06:27 +0600

    end and finish

--- src/Components/ControlButtons/ControlButtons.jsx
+++ src/Components/ControlButtons/ControlButtons.jsx
@@ -20,7 +20,7 @@ export default function ControlButtons(props) {
             </div>
             <div className="btn btn-one"
                 onClick={props.handlePauseResume}>
-                {props.isPaused ? "Resume" : "Pause"}
+                {props.isPaused ? "Resume" : "Stop"}
             </div>
         </div>
     );
